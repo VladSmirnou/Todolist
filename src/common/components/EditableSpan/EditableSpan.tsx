37a2faf37a1d475e448420c0b1509b475115431b
VladSmirnou <smr.vladislav@gmail.com>
2024-12-02 22:16:15 +0300

    feat: add functional editable span

--- src/common/components/EditableSpan/EditableSpan.tsx
+++ src/common/components/EditableSpan/EditableSpan.tsx
@@ -1,12 +1,38 @@
-import { AddItemForm } from '../AddItemForm/AddItemForm';
+import { ChangeEvent, useState } from 'react';
 
 type Props = {
     spanText: string;
+    onEdit: (newValue: string) => void;
 };
 
 export const EditableSpan = (props: Props) => {
-    const { spanText } = props;
+    const { spanText, onEdit } = props;
 
-    const editing = false;
-    return editing ? <AddItemForm /> : <span>{spanText}</span>;
+    const [inputText, setInputText] = useState('');
+    const [error, setError] = useState('');
+    const [editMode, setEditMode] = useState(false);
+
+    const handleSetEditModeOn = () => {
+        setInputText(spanText);
+        setEditMode(true);
+    };
+
+    const handleChange = (e: ChangeEvent<HTMLInputElement>) => {
+        setInputText(e.target.value);
+    };
+
+    const handleBlur = () => {
+        setEditMode(false);
+        onEdit(inputText);
+    };
+
+    return editMode ?
+            <input
+                type="text"
+                value={inputText}
+                onChange={handleChange}
+                onBlur={handleBlur}
+                autoFocus
+            />
+        :   <span onDoubleClick={handleSetEditModeOn}>{spanText}</span>;
 };
